0a67e2d412ce3303a00ded6f2cab08968fa1a916
Freemason-EAG <egoyan.arutyun@mail.ru>
2025-07-13 15:51:00 +0300

    add even-games and brain-even

---
+++ src/even-game.js
@@ -0,0 +1,64 @@
+import readlineSync from "readline-sync";
+import askName from "../src/cli.js";
+
+const random = () => {
+
+    return Math.floor(Math.random() * 100) + 1;
+
+};
+
+
+const parityCheck = (numbers) => {
+
+    let result = [];
+    for (const num of numbers) {
+
+        if (num % 2 === 0) {
+
+            result.push("yes");
+
+        } else {
+
+            result.push("no");
+
+        }
+
+    }
+    return result;
+
+};
+
+
+const evenGame = () => {
+
+    const name = askName();
+    const numbers = [
+        random(),
+        random(),
+        random()
+    ];
+    const trueAnswers = parityCheck(numbers);
+    console.log("Answer \"yes\" if the number is even, otherwise answer \"no\".");
+    for (let i = 0; i < trueAnswers.length; i++) {
+
+        const num = numbers[i];
+        const answer = readlineSync.question(`Question: ${num}`);
+        console.log(`Your answer: ${answer}`);
+        if (answer === trueAnswers[i]) {
+
+            console.log("Correct!");
+
+        } else {
+
+            console.log(`${answer} is wrong answer ;(. Correct answer was ${trueAnswers[i]}.\nLet's try again, ${name}!`);
+
+        }
+
+    }
+    console.log(`Congratulations, ${name}!`);
+
+};
+
+
+export default evenGame;
+
